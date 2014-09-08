aa555cc8155d6fd2fb59402b78191c46f94cc1c0
wychootf4 <hootf4@gmail.com>
2014-09-08 15:50:56 -0400

    EvaluateReversePolishNotation
    1st commit

---
+++ StringArrayMatrix/EvaluateReversePolishNotation.java
@@ -0,0 +1,47 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+*/
+import java.util.Stack;
+public class EvaluateReversePolishNotation
+{
+	public static void main(String args[]){
+		String[] tokens = {"2","1","+","3","*"};
+		System.out.println(evalRPN(tokens));
+	}
+	public static int evalRPN(String[] tokens){
+		int returnValue = 0;
+		String operator = "+-*/";
+		Stack<String> stack = new Stack<String>();
+		for (int i : tokens)
+		{
+			if (!operator.contains(i))
+			{
+				stack.push(i);
+			}else{
+				int a = Integer.valueOf(stack.pop());
+				int b = Integer.valueOf(stack.pop());
+				int index = operator.indexOf(i);
+				switch(index){
+					case 0:
+						stack.push(String.valueOf(b + a));
+						break;
+					case 1:
+						stack.push(String.valueOf(b - a));
+						break;
+					case 2:
+						stack.push(String.valueOf(b * a));
+						break;
+					case 3:
+						stack.push(String.valueOf(b / a));
+						break;
+				}
+			}
+		}
+		returnValue = Integer.valueOf(stack.pop());
+		return returnValue;
+	}
+}
