e20c6b062bdc249f973903eb9981fb8720172c0c
wychootf4 <Raymond@172-26-204-185.dynapool.nyu.edu>
2014-10-14 16:03:25 -0400

    10/14 update

---
+++ BinarySearch/34_H_SearchForARange.java
@@ -0,0 +1,62 @@
+/*
+Given a sorted array of integers, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].
+*/
+
+public class Solution {
+    public int[] searchRange(int[] A, int target) {
+        int start, end, mid;
+        int[] range = new int[2];
+
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target){
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[start] == target) {
+            range[0] = start;
+        } else if (A[end] == target) {
+            range[0] = end;
+        } else {
+            range[0] = range[1] = -1;
+            return range;
+        }
+
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                start = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[end] == target) {
+            range[1] = end;
+        } else if (A[start] == target) {
+            range[1] = start;
+        } else {
+            range[0] = range[1] = -1;
+            return range;
+        }
+
+        return range;
+    }
+}
--- BinarySearch/Model.java
+++ BinarySearch/Model.java
@@ -5,6 +5,7 @@ If the target number does not exist in the array, return -1.
 Example If the array is [1, 2, 3, 3, 4, 5, 10], for given target 3, return 2.
 */
 
+// 用while loop显得比recursion好一些，另外recursion容易造成错误，适当情况再用
 public class Solution{
     public int binarySearch(int[] nums, int target){
         if (nums.length == 0){
@@ -14,18 +15,32 @@ public class Solution{
         int start = 0;
         int end = nums.length - 1;
         int mid;
-
+        /*
+         这里用(start < end)的话容易造成死循环，比如start = 1，end = 2，进入循环后mid = 1，而此时如果
+         nums[mid] < target，则start = mid = 1，会进入死循环
+         用(start + 1 < end)的话while循环结束后start和end是邻接点或是重合点
+        */
         while (start + 1 < end){
+            // 这样计算mid可以防止start + end大于int范围溢出
             mid = start + (end - start) / 2;
             if (nums[mid] == target){
+                /*
+                 为什么不直接return mid？因为题目要求返回数组中第一个等于target的元素，如果mid前还有等于target
+                 的元素那么直接return的结果就是错的。将mid赋给end是为了保证while循环结束后在进行比较时如果mid前
+                 有元素符合条件可以先返回其index, end = mid至少可以保证找到的index最末也是在mid位置
+                */
                 end = mid;
             } else if (nums[mid] < target) {
+                // 为什么不用start = mid + 1?对于某些题不适用
                 start = mid;
             } else if (nums[mid] > target) {
                 end = mid;
             }
         }
-
+        /*
+         因为start和end是一个点或是邻接点，则如果target在数组中存在必然是两点之一
+         由于是要找到第一次出现的index，所以先判断start
+        */
         if (nums[start] == target){
             return start;
         }
