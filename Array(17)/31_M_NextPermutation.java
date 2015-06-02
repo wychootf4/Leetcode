e85adfd159b501a145693dfe8401717bc001509f
wychootf4 <hootf4@gmail.com>
2015-06-02 02:30:27 -0700

    6/1 update

--- Array(17)/31_M_NextPermutation.java
+++ Array(17)/31_M_NextPermutation.java
@@ -14,3 +14,53 @@ column.
 1,1,5 → 1,5,1
 */
 // Tag: Array
+
+/*
+分析：
+题目需要找到下一个permutation，也就是恰好比现在的序列大的序列中最小的一个。
+1.所以先要从右向左扫到第一个下降的元素，即dropIndex，可以用单词去类比想象，肯定是要在允许的范围内尽可能的变动右边的元素，
+这样才能保证是比现序列大的序列中最小的一个。
+2.由于dropIndex后的元素是降序，不可能再增大了，所以要变动dropIndex。还是从右向左扫描找到第一个比dropIndex大的元素与其交换，
+这样保证序列最小。
+3.交换后由于dropIndex位置的值变大了，相应的后面的序列要整体逆序变为升序，这样才能保证序列最小。
+4.如果第1步的时候就没找到dropIndex，直接将序列逆序即可。
+*/
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return;
+        }
+
+        for (int i = nums.length - 2; i >= 0; i--){
+            // 如果当前的比后面的一个小，就找到其后面从右到左第一个比当前元素大的元素，两者交换，然后将当前元素位置
+            // 后面的元素全部逆序
+            if (nums[i] < nums[i + 1]){
+                int j = 0;
+                for (j = nums.length - 1; j > i; j--){
+                    if (nums[j] > nums[i]){
+                        swap(nums, i, j);
+                        break;
+                    }
+                }
+                reverse(nums, i + 1, nums.length - 1);
+                return;
+            }
+        }
+
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end){
+        while (start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
--- Array(17)/48_M_RotateImage.java
+++ Array(17)/48_M_RotateImage.java
@@ -7,3 +7,38 @@ Follow up:
 Could you do this in-place?
 */
 // Tag: Array
+
+/*
+分析：
+网上的答案不太好记忆，这里采用规定上下左右边界的办法，上和左为0点，从外到内每次循环处理一圈，往内圈走边长减2.
+*/
+public class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return;
+        }
+
+        int n = matrix.length;
+        int top = 0;
+        int down = n - 1;
+        int left = 0;
+        int right = n - 1;
+        // 至少边长为2才需要翻转
+        while (n > 1){
+            // 由于每次翻转时候都是从四个角开始，一条边上有两个角，所以实际每条边就是翻转n - 1次。并且翻转时候按顺时针顺序处理。
+            for (int i = 0; i < n - 1; i++){
+                int temp = matrix[top][left + i];
+                matrix[top][left + i] = matrix[down - i][left];
+                matrix[down - i][left] = matrix[down][right - i];
+                matrix[down][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = temp;
+            }
+
+            top++;
+            down--;
+            left++;
+            right--;
+            n -= 2;
+        }
+    }
+}
