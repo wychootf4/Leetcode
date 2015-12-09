f82c7b4d2c10ffd4164f6f6cd21d45671978eb06
wychootf4 <hootf4@gmail.com>
2015-12-08 23:50:10 -0800

    12/08/2015 update

--- DynamicProgramming(29)/10_H_RegularExpressionMatching.java
+++ DynamicProgramming(29)/10_H_RegularExpressionMatching.java
@@ -19,3 +19,29 @@ isMatch("ab", ".*") → true
 isMatch("aab", "c*a*b") → true
 */
 // Tag: Dynamic Programming, Backtracking, String
+
+/*
+题意解析:注意这里*代表前面的一个字符出现0次或多次,比如.*表示.出现0次或多次,多个.可以匹配任一字符.
+*/
+// Solution1: 利用递归实现,真正匹配的条件为两个字符相同或是字符与'.'匹配. 如果当前p字符不是*,判断是否匹配,如果是*则
+// 枚举所有情况.
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        // base case
+        if (p.length() == 0) {
+            return s.length() == 0;
+        }
+
+        //
+        if (p.length() == 1) {
+            if (s.length() < 1) {
+                return false;
+                // first not match
+            } else {
+                return matchChar(s.charAt(0), p.charAt(0));
+            }
+        }
+    }
+
+
+}
