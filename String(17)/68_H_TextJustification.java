54fe2779d1d4cac515f3dab6941178731b4c48ba
wychootf4 <hootf4@gmail.com>
2015-12-16 01:26:38 -0800

    12/15/2015 update

--- String(17)/68_H_TextJustification.java
+++ String(17)/68_H_TextJustification.java
@@ -1,13 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /*
-Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
+Given an array of words and a length L, format the text such that each line has exactly L characters
 
-You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly L characters.
+and is fully (left and right) justified. You should pack your words in a greedy approach; that is,
 
-Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line
 
-For the last line of text, it should be left justified and no extra space is inserted between words.
+has exactly L characters. Extra spaces between words should be distributed as evenly as possible.
+
+If the number of spaces on a line do not divide evenly between words, the empty slots on the left
+
+will be assigned more spaces than the slots on the right. For the last line of text, it should be
+
+left justified and no extra space is inserted between words.
 
 For example,
 words: ["This", "is", "an", "example", "of", "text", "justification."]
@@ -28,6 +35,74 @@ In this case, that line should be left-justified.
 // Tag: String
 public class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<String>();
+        if (words == null || maxWidth < 0) {
+            return res;
+        }
+        // 标记处理到哪个单词
+        int startIndex = 0;
+        while (startIndex < words.length) {
+            int charCount = maxWidth;
+            int endIndex = startIndex;
+            // 对于每个仍能放入本行的单词
+            for (int i = startIndex; i < words.length && words[i].length() <= charCount; i++) {
+                charCount -= words[i].length();
+                // 计算该单词后面跟着的空格
+                charCount--;
+                endIndex = i;
+            }
+            // 收回最后一个空格,最后统一处理
+            charCount++;
+            // 计算本行放了几个单词
+            int wordCount = endIndex - startIndex + 1;
 
+            int extraSpace = 0;
+            int firstExtra = 0;
+            // 如果单词数大于1,分配空格,否则都加到最后即可
+            if (wordCount > 1) {
+                // n个单词中间有n-1个空可以加空格,每个单词后面多余可加的空格数
+                extraSpace = charCount / (wordCount - 1);
+                // 均分后富裕出来的空格数
+                firstExtra = charCount % (wordCount - 1);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            // 对于本行的单词
+            for (int i = startIndex, i <= endIndex; i++) {
+                sb.append(words[i]);
+                int spaceCount = 0;
+                // 如果是最后一个单词后面不加空格
+                if (i == endIndex) {
+                    break;
+                }
+                // 如果不是最后一行
+                if (endIndex != words.length - 1) {
+                    if (firstExtra > 0) {
+                        spaceCount++;
+                        firstExtra--;
+                    }
+                    // 不是最后一个单词,可以加额外空格
+                    spaceCount += extraSpace;
+                }
+
+                // 1是每个单词后面本来要加的空格
+                appendSpace(sb, spaceCount + 1);
+            }
+
+            // 最后一行的尾部的空格,或是只有一个单词的情况
+            appendSpace(sb, maxWidth - sb.length());
+
+            res.add(sb.toString());
+            startIndex = endIndex + 1;
+        }
+
+        return res;
+    }
+
+    public void appendSpace(StringBuilder sb, int spaceCount) {
+        while (spaceCount > 0) {
+            sb.append(' ');
+            spaceCount--;
+        }
     }
 }
--- String(17)/71_M_SimplifyPath.java
+++ String(17)/71_M_SimplifyPath.java
@@ -12,3 +12,54 @@ Another corner case is the path might contain multiple slashes '/' together, suc
 In this case, you should ignore redundant slashes and return "/home/foo".
 */
 // Tag: Stack, String
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Solution:
+首先需要说明简化路径的依据:
+1.遇到/../需要返回上一级目录,需检查上一层目录是否为空
+2.遇到/./表示是本级目录,简化冗余
+3.遇到//表示是本级目录,简化冗余
+4.遇到其他字符表示是文件夹名,无需简化
+5.字符串为空或者遇到/../,需要返回一个/
+首先将字符串用/进行分割,如果当前字符串为..则去掉前面已经存入path的目录,如果不为.或是空则加入path.然后为path分别加/,
+最后判断result是否为空,如果不为空去掉上一步加到最后的/
+ */
+public class Solution {
+    public String simplifyPath(String path) {
+        if (path == null || path.length() == 0);
+
+        // 用/将path分割,/+代表一个或多个/
+        String[] stubs = path.split("/+");
+        List<String> list = new ArrayList<String>();
+
+        for (String s : stubs) {
+            // 如果为..需要返回上一级目录,则去掉前面已经存入path的目录
+            if (s.equals("..")) {
+                if (list.size() > 0) {
+                    list.remove(list.size() - 1);
+                }
+                // 如果不为.而且不为空,则加入path,否则不作处理
+            } else if (!s.equals(".") && !s.equals("")) {
+                list.add(s);
+            }
+        }
+
+        // 加/
+        // 这里用stringbuilder,减少空间的使用量
+        StringBuilder sb = new StringBuilder();
+        sb.append("/");
+
+        for (String s : list) {
+             sb.append(s + "/");
+        }
+        // 如果结果不为空则去掉加在末尾的/
+        if (sb.length() > 1) {
+            sb = sb.deleteCharAt(sb.length() - 1);
+        }
+
+        return sb.toString();
+    }
+}
