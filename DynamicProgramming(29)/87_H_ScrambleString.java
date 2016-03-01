099e66b291ccb5db9a5fc08121f68ac09f719a3c
wychootf4 <hootf4@gmail.com>
2016-02-29 18:56:21 -0800

    last modify

--- DynamicProgramming(29)/87_H_ScrambleString.java
+++ DynamicProgramming(29)/87_H_ScrambleString.java
@@ -1,5 +1,6 @@
 /*
-Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty
+substrings recursively.
 
 Below is one possible representation of s1 = "great":
 
@@ -12,7 +13,8 @@ g   r  e   at
           a   t
 To scramble the string, we may choose any non-leaf node and swap its two children.
 
-For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+For example, if we choose the node "gr" and swap its two children, it produces a scrambled string
+"rgeat".
 
     rgeat
    /    \
@@ -23,7 +25,8 @@ r   g  e   at
           a   t
 We say that "rgeat" is a scrambled string of "great".
 
-Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".
+Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled
+string "rgtae".
 
     rgtae
    /    \
