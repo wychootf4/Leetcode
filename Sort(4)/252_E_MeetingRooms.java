762bb8e9ce36090a4f3b24f68249ab941a47e347
Yuchen Wang <Yuchen Wang>
2016-08-28 23:26:57 +0100

    Add meeting rooms

---
+++ Sort(4)/252_E_MeetingRooms.java
@@ -0,0 +1,44 @@
+/*
+Given an array of meeting time intervals consisting of start and end times
+[[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return false.
+*/
+// Company: Facebook
+// Tag: Sort
+
+/*
+思路：利用Comparator来根据每个interval的start进行排序，然后如果如果有overlap则不符合rule
+*/
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return true;
+        }
+
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
+        });
+
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i + 1].start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
